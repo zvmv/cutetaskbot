9a071559833a5315b1ef8f11f8ab14399b0c3669
zvmv <zvonilov_kst@mts-nn.ru>
2021-12-20 22:17:31 +0300

    Chore

--- src/test/java/ru/pet/cutetaskbot/UtilTest.java
+++ src/test/java/ru/pet/cutetaskbot/UtilTest.java
@@ -3,8 +3,6 @@ package ru.pet.cutetaskbot;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
